68da75254e9c8807fadeb687643bcb7f47514a1b
Tiogiras <tiogiras@gmail.com>
2024-03-10 19:54:11 +0100

    Code CleanUp

--- Editor/Scripts/ContextMenu.cs
+++ Editor/Scripts/ContextMenu.cs
@@ -14,18 +14,6 @@ internal static partial class ContextMenu
 {
     #region Private Methods
 
-    [MenuItem(MenuItemPackages + "/Screenshot/Window Capture", false, 100)]
-    private static void OpenWindowCapture()
-    {
-        TryOpen <WindowCapture>(false);
-    }
-    
-    [MenuItem(MenuItemPackages + "/Screenshot/Shortcut Capture", false, 101)]
-    private static void OpenShortcutCapture()
-    {
-        TryOpen <ShortcutCapture>(false);
-    }
-    
     [MenuItem(MenuItemPackages + "/Screenshot/Capture Now", false, 102)]
     private static void CaptureNow()
     {
@@ -39,9 +27,10 @@ internal static partial class ContextMenu
         if (activeCams is not {Count: > 0})
         {
             EditorUtility.DisplayDialog("Missing Cameras", "No CameraCapture components selected for rendering.", "Ok");
+
             return;
         }
-        
+
         foreach (CameraCapture cam in activeCams)
         {
             var timestamp = $"{DateTime.Now:yy-MM-dd})({DateTime.Now:HH-mm-ss}";
@@ -49,11 +38,22 @@ internal static partial class ContextMenu
             var path = $"{cam.lastPath}{camName}";
             cam.RenderAndSave(path);
         }
-        
+
         Debug.Log($"{activeCams.Count} CameraCapture components rendered.");
     }
 
-    
+    [MenuItem(MenuItemPackages + "/Screenshot/Shortcut Capture", false, 101)]
+    private static void OpenShortcutCapture()
+    {
+        TryOpen <ShortcutCapture>(false);
+    }
+
+    [MenuItem(MenuItemPackages + "/Screenshot/Window Capture", false, 100)]
+    private static void OpenWindowCapture()
+    {
+        TryOpen <WindowCapture>(false);
+    }
+
     [MenuItem("Window/MegaPint/Test", false, 0)]
     private static void Test()
     {
--- Editor/Scripts/DisplayContent.cs
+++ Editor/Scripts/DisplayContent.cs
@@ -1,10 +1,4 @@
 ﻿#if UNITY_EDITOR
-using System;
-using Editor.Scripts.Settings;
-using Editor.Scripts.Windows;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Editor.Scripts
 {
