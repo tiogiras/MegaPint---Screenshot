1aa5648ee226c76d5d61248a9598345fca14a84d
Tiogiras <tiogiras@gmail.com>
2024-03-10 20:23:12 +0100

    GUI WIP

--- Editor/Scripts/DisplayContent.cs
+++ Editor/Scripts/DisplayContent.cs
@@ -1,11 +1,41 @@
-﻿#if UNITY_EDITOR
+﻿using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
 
 namespace Editor.Scripts
 {
 
 internal static partial class DisplayContent
 {
+    private const string BasePathScreenshot = "Screenshot/User Interface/Display Content Tabs/";
     
+    private static void UnsubscribeScreenshot()
+    {
+        s_onSelectedTabChanged -= OnTabChangedScreenshot;
+        s_onSelectedPackageChanged -= UnsubscribeScreenshot;
+    }
+    
+    // Called by reflection
+    // ReSharper disable once UnusedMember.Local
+    private static void Screenshot(VisualElement root)
+    {
+        var tabs = root.Q <GroupBox>("Tabs");
+        var tabContentParent = root.Q <GroupBox>("TabContent");
+
+        RegisterTabCallbacks(tabs, tabContentParent, 3);
+
+        SetTabContentLocations(BasePathScreenshot + "Tab0", BasePathScreenshot + "Tab1", BasePathScreenshot + "Tab2");
+
+        s_onSelectedTabChanged += OnTabChangedScreenshot;
+        s_onSelectedPackageChanged += UnsubscribeScreenshot;
+
+        SwitchTab(tabContentParent, 0);
+    }
+    
+    private static void OnTabChangedScreenshot(int tab, VisualElement root)
+    {
+
+    }
 }
 
 }
